aa90c8e3ba95437b801b03b48a08557a28a4e6bb
Ruohao <ruohaolin@gmail.com>
2023-12-27 22:41:04 +0100

    random secret key

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, text
 import os
+import secrets
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = secrets.token_hex(32)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
