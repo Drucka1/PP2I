075fdf9288d2b12a7b5baf7f1bae0bd7b83456a6
Ruohao <ruohaolin@gmail.com>
2024-01-16 15:57:41 +0100

    a bit of css

--- web/app/routes.py
+++ web/app/routes.py
@@ -2,9 +2,69 @@ from flask import render_template, request, redirect, session, url_for
 from app import app
 from app.aux import get_db, get_min_recettes, get_full_recette, hash, get_recette_realisable_user, creation_menu, get_toutes_recettes, get_ustensiles, get_allergenes  
 
-@app.route('/')
-def hello_world():
-    return index()
+@app.route('/',methods=['POST','GET'])
+@app.route('/home',methods=['POST','GET'])
+def home():
+    if request.method == "POST" and 'user' in session:
+        result = request.form.to_dict()
+        if 'nom_recette' in result and 'change_fav' in result :
+            if 'fav' in result:
+                e = get_db().cursor()
+                e.execute("SELECT * FROM favori WHERE id_recette="+str(result['id_recette'])+" and id_user="+str(session['id']))
+                
+                if len(list(e)) == 0:
+                    d = get_db().cursor()
+                    d.execute("INSERT INTO favori (id_recette,id_user) VALUES ("+str(result['id_recette'])+","+str(session['id'])+")")
+                    get_db().commit()
+                    session['favori'].append(int(result['id_recette']))
+                    session.modified = True
+
+            else :
+                d = get_db().cursor()
+                d.execute("DELETE FROM favori WHERE id_recette="+str(result['id_recette'])+" and id_user="+str(session['id']))
+                get_db().commit()
+                session['favori'].remove(int(result['id_recette']))
+                session.modified = True
+        
+            
+            c = get_db().cursor()
+            c.execute("SELECT * FROM recipes WHERE title LIKE '%"+str(result['nom_recette'])+"%'")
+            return render_template('home.html',search=str(result['nom_recette']),recettes=get_min_recettes(c))
+        
+        
+        elif 'nom_recette' in result :
+            c = get_db().cursor()
+            c.execute("SELECT * FROM recipes WHERE title LIKE '%"+str(result['nom_recette'])+"%'")
+            return render_template('home.html',search=str(result['nom_recette']),recettes=get_min_recettes(c))
+            
+        elif 'change_fav' in result :
+            
+            if 'fav' in result:
+                e = get_db().cursor()
+                e.execute("SELECT * FROM favori WHERE id_recette="+str(result['id_recette'])+" and id_user="+str(session['id']))
+                
+                if len(list(e)) == 0:
+                    d = get_db().cursor()
+                    d.execute("INSERT INTO favori (id_recette,id_user) VALUES ("+str(result['id_recette'])+","+str(session['id'])+")")
+                    get_db().commit()
+                    session['favori'].append(int(result['id_recette']))
+                    session.modified = True
+
+            else :
+                d = get_db().cursor()
+                d.execute("DELETE FROM favori WHERE id_recette="+str(result['id_recette'])+" and id_user="+str(session['id']))
+                get_db().commit()
+                session['favori'].remove(int(result['id_recette']))
+                session.modified = True
+            
+            
+            c = get_db().cursor()
+            c.execute("SELECT * FROM recipes LIMIT 50")
+            return redirect(url_for('home',_anchor=str(result['id_recette']),recettes=get_min_recettes(c)))
+        
+    c = get_db().cursor()
+    c.execute("SELECT * FROM recipes LIMIT 50")
+    return render_template('home.html', recettes = get_min_recettes(c))
 
 @app.route('/recipe/<int:recipeId>',methods=['POST','GET'])
 def recipe(recipeId):
@@ -36,68 +96,6 @@ def recipe(recipeId):
     
     return render_template('recipe.html',recette=get_full_recette(c))
 
-@app.route('/index',methods=['POST','GET'])
-def index():
-    if request.method == "POST" and 'user' in session:
-        result = request.form.to_dict()
-        if 'nom_recette' in result and 'change_fav' in result :
-            if 'fav' in result:
-                e = get_db().cursor()
-                e.execute("SELECT * FROM favori WHERE id_recette="+str(result['id_recette'])+" and id_user="+str(session['id']))
-                
-                if len(list(e)) == 0:
-                    d = get_db().cursor()
-                    d.execute("INSERT INTO favori (id_recette,id_user) VALUES ("+str(result['id_recette'])+","+str(session['id'])+")")
-                    get_db().commit()
-                    session['favori'].append(int(result['id_recette']))
-                    session.modified = True
-
-            else :
-                d = get_db().cursor()
-                d.execute("DELETE FROM favori WHERE id_recette="+str(result['id_recette'])+" and id_user="+str(session['id']))
-                get_db().commit()
-                session['favori'].remove(int(result['id_recette']))
-                session.modified = True
-        
-            
-            c = get_db().cursor()
-            c.execute("SELECT * FROM recipes WHERE title LIKE '%"+str(result['nom_recette'])+"%'")
-            return render_template('index.html',search=str(result['nom_recette']),recettes=get_min_recettes(c))
-        
-        
-        elif 'nom_recette' in result :
-            c = get_db().cursor()
-            c.execute("SELECT * FROM recipes WHERE title LIKE '%"+str(result['nom_recette'])+"%'")
-            return render_template('index.html',search=str(result['nom_recette']),recettes=get_min_recettes(c))
-            
-        elif 'change_fav' in result :
-            
-            if 'fav' in result:
-                e = get_db().cursor()
-                e.execute("SELECT * FROM favori WHERE id_recette="+str(result['id_recette'])+" and id_user="+str(session['id']))
-                
-                if len(list(e)) == 0:
-                    d = get_db().cursor()
-                    d.execute("INSERT INTO favori (id_recette,id_user) VALUES ("+str(result['id_recette'])+","+str(session['id'])+")")
-                    get_db().commit()
-                    session['favori'].append(int(result['id_recette']))
-                    session.modified = True
-
-            else :
-                d = get_db().cursor()
-                d.execute("DELETE FROM favori WHERE id_recette="+str(result['id_recette'])+" and id_user="+str(session['id']))
-                get_db().commit()
-                session['favori'].remove(int(result['id_recette']))
-                session.modified = True
-            
-            
-            c = get_db().cursor()
-            c.execute("SELECT * FROM recipes LIMIT 50")
-            return redirect(url_for('index',_anchor=str(result['id_recette']),recettes=get_min_recettes(c)))
-        
-    c = get_db().cursor()
-    c.execute("SELECT * FROM recipes LIMIT 50")
-    return render_template('index.html', recettes = get_min_recettes(c))
 
 @app.route('/login',methods=['POST','GET'])
 def login():
@@ -137,19 +135,19 @@ def login():
                     if elt[1] != None:
                         session[elt[0]] = elt[1]
                    
-            return redirect('/index')
+            return redirect('/home')
         else : 
             return render_template('login.html',error='Incorrect password or username')
     else: 
         if 'user' in session:
-            return redirect("/index")   
+            return redirect("/home")   
         else:
             return render_template("login.html")
 
 @app.route('/logout',methods=['POST','GET'])
 def logout():
     session.clear()
-    return redirect("/index")
+    return redirect("/home")
     
 @app.route('/register',methods=['POST','GET'])
 def register():
@@ -208,7 +206,7 @@ def register():
         
     else:
         if 'user' in session:
-            return redirect("/index")   
+            return redirect("/home")   
         else:
             return render_template("register.html") 
 
@@ -316,7 +314,7 @@ def choix():
     
     else:
         if 'user' not in session:
-            return redirect('/index')
+            return redirect('/home')
         else:
             return render_template("choice.html",allergenes=get_allergenes(),ustensiles=get_ustensiles() )
 
